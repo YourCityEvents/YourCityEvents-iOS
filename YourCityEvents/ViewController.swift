f5a09b2f711a1b16e5551764408d7242fb230535
Yaroslav Zarichnyi <yarik120700@gmail.com>
2019-10-21 01:54:55 +0300

    rep updated

---
+++ YourCityEvents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YourCityEvents
+//
+//  Created by Yaroslav Zarechnyy on 10/21/19.
+//  Copyright © 2019 Yaroslav Zarechnyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
